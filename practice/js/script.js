2cd99f9409baea974424df4db4060b82e58737de
tkachenko6591 <tkachenko6591@gmail.com>
2025-07-11 23:30:45 -0300

    add: practice

---
+++ practice/js/script.js
@@ -0,0 +1,31 @@
+// Строгий режим
+"use strict"
+
+
+// let user = "admin"
+// let userAge
+// if (user === "admin") {
+// 	userAge = 34
+// }
+// if (userAge === undefined) {
+// 	console.log('Error')
+// } else {
+// 	console.log(userAge)
+// }
+
+
+
+// let number = 10 / "АНАСТАСІЯ"
+// console.log(typeof number) // Тип даних
+// console.log(number) // Значення
+
+// if (isNaN(number)) {
+// 	console.log('Це NaN')
+// }
+
+
+// let userName = `Valik`
+// let userLastName = `Tkachenko`
+// let userAge = 34
+// let userInfo = `Name: ${userName}, Last Name: ${userLastName}, Age: ${userAge}`
+// console.log(userInfo)
